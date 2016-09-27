e565bc644b7bc8b9b0b2b42bb69a7d839270dccf
Raymond Cano <raymond@Raymonds-MacBook-Pro.local>
2016-09-27 13:37:02 -0500

    working conv net

--- hw1/dcn_mnist.py
+++ hw1/dcn_mnist.py
@@ -53,7 +53,7 @@ def conv2d(x, W):
     :return: a tensor of features extracted by the filters, a.k.a. the results after convolution
     '''
     # IMPLEMENT YOUR CONV2D HERE
-    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
     '''
@@ -62,7 +62,8 @@ def max_pool_2x2(x):
     :return: the results of maxpooling (max-marginalized + downsampling)
     '''
     # IMPLEMENT YOUR MAX_POOL_2X2 HERE
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
 
 def main():
     # Specify training parameters
@@ -77,7 +78,7 @@ def main():
 
     # placeholders for input data and input labeles
     x = tf.placeholder(tf.float32, shape=[None, SIZE])
-    y_ = tf.placeholder(tf.float32, shape = [None, NUM_CLASSES])
+    y_ = tf.placeholder(tf.float32, shape=[None, NUM_CLASSES])
 
     # reshape the input image
     x_image = tf.reshape(x, [-1, 28, 28, 1])
@@ -85,13 +86,13 @@ def main():
     # first convolutional layer
     W_conv1 = weight_variable([5,5,1,32])
     b_conv1 = bias_variable([32])
-    h_conv1 = conv2d(x, W_conv1) + b_conv1
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
     # second convolutional layer
     W_conv2 = weight_variable([5,5,32,64])
-    b_conv2 = bias_variable([32])
-    h_conv2 = conv2d(x, W_conv2) + b_conv2
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
     # densely connected layer
@@ -101,7 +102,7 @@ def main():
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
-    keep_prob = .5
+    keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob=keep_prob)
 
     # softmax
@@ -114,7 +115,7 @@ def main():
     # setup training
     cross_entropy = tf.reduce_mean(-1 * tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1)))
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     # Add a scalar summary for the snapshot loss.
@@ -123,7 +124,7 @@ def main():
     summary_op = tf.merge_all_summaries()
 
     # Add the variable initializer Op.
-    init = tf.initialize_all_variables()
+    # init = tf.initialize_all_variables()
 
     # Create a saver for writing training checkpoints.
     saver = tf.train.Saver()
@@ -132,13 +133,14 @@ def main():
     summary_writer = tf.train.SummaryWriter(result_dir, sess.graph)
 
     # Run the Op to initialize the variables.
-    sess.run(init)
+    sess.run(tf.initialize_all_variables())
 
     # run the training
     for i in range(max_step):
         batch = mnist.train.next_batch(50) # make the data batch, which is used in the training iteration.
                                             # the batch size is 50
         if i%100 == 0:
+
             # output the training accuracy every 100 iterations
             train_accuracy = accuracy.eval(feed_dict={
                 x:batch[0], y_:batch[1], keep_prob: 1.0})
