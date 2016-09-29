1f813aba8596ac780eca4016d287bbd405459764
Raymond Cano <rfc1@rice.edu>
2016-09-29 11:25:19 -0500

    added summary code

--- hw1/dcn_mnist.py
+++ hw1/dcn_mnist.py
@@ -10,6 +10,7 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 # Import Tensorflow and start a session
 import tensorflow as tf
 sess = tf.InteractiveSession()
+RUN = '/run1'
 
 def weight_variable(shape):
     '''
@@ -65,6 +66,55 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
+def nn_layer(input_tensor, input_channel, output_channel, window, name):
+    """
+    Generalized conv layer with a convolution and nonlinearity
+    :param input_tensor: input tensor that will be multiplied with
+    :param input_channel: dimension of the input channel
+    :param output_channel: dimension of the output channel
+    :param window: window size
+    :param name: name of layer
+    :return: output of the run
+    """
+    add_summaries(input_tensor, name + '.input')
+    w_conv = weight_variable([window, window, input_channel, output_channel])
+    add_summaries(w_conv, name + '.w')
+    b_conv = bias_variable([output_channel])
+    add_summaries(b_conv, name + '.b')
+    h_conv = tf.nn.relu(conv2d(input_tensor, w_conv) + b_conv)
+    add_summaries(h_conv, name+'.activation')
+    pooled = max_pool_2x2(h_conv)
+    add_summaries(pooled, name+'.maxpool')
+    return pooled
+
+def add_summaries(tensor, name):
+    """
+    add the data to the summary writer
+    :param tensor: tensor to analyze
+    :param name: name of tensor
+    :return: None
+    """
+    name = name
+    mean = tf.reduce_mean(tensor)
+    tf.scalar_summary(name + '.mean', mean)
+    std = tf.sqrt(tf.reduce_mean(tf.square(tensor - mean)))
+    tf.scalar_summary(name + '.std', std)
+    tf.scalar_summary(name + '.max', tf.reduce_max(tensor))
+    tf.scalar_summary(name + '.min', tf.reduce_min(tensor))
+    tf.histogram_summary(name, tensor)
+
+def get_accuracies(accuracy, x, y_, keep_prob):
+    """
+    get the validation and test accuracies
+    :return:
+    """
+    test_acc = accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+    val_acc = accuracy.eval(feed_dict={
+        x: mnist.validation.images, y_: mnist.validation.labels, keep_prob: 1.0})
+
+    return test_acc, val_acc
+
 def main():
     # Specify training parameters
     result_dir = './results/' # directory where the results from the training are saved
@@ -84,21 +134,15 @@ def main():
     x_image = tf.reshape(x, [-1, 28, 28, 1])
 
     # first convolutional layer
-    W_conv1 = weight_variable([5,5,1,32])
-    b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+    layer_1 = nn_layer(x_image, 1, 32, 5, 'layer_1')
 
     # second convolutional layer
-    W_conv2 = weight_variable([5,5,32,64])
-    b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    layer_2 = nn_layer(layer_1, 32, 64, 5, 'layer_2')
 
     # densely connected layer
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_pool2_flat = tf.reshape(layer_2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
@@ -120,6 +164,11 @@ def main():
 
     # Add a scalar summary for the snapshot loss.
     tf.scalar_summary(cross_entropy.op.name, cross_entropy)
+    sess.run(tf.initialize_all_variables())
+    test_acc, val_acc = get_accuracies(accuracy, x, y_, keep_prob)
+    test_sum = tf.scalar_summary('test_error', test_acc)
+    val_sum = tf.scalar_summary('validation_error', val_acc)
+
     # Build the summary operation based on the TF collection of Summaries.
     summary_op = tf.merge_all_summaries()
 
@@ -130,32 +179,51 @@ def main():
     saver = tf.train.Saver()
 
     # Instantiate a SummaryWriter to output summaries and the Graph.
-    summary_writer = tf.train.SummaryWriter(result_dir, sess.graph)
-
+    train_writer = tf.train.SummaryWriter(result_dir + RUN + '/train', sess.graph)
+    error_writer = tf.train.SummaryWriter(result_dir + RUN + '/error')
     # Run the Op to initialize the variables.
-    sess.run(tf.initialize_all_variables())
 
-    # run the training
     for i in range(max_step):
         batch = mnist.train.next_batch(50) # make the data batch, which is used in the training iteration.
-                                            # the batch size is 50
-        if i%100 == 0:
+
+        # Define here to capture scoping
+        def feed_dict(test=False, validation=False):
+            """
+            assign the proper params to the feed dict give that we're looking for test or validation
+            :param test:
+            :return:
+            """
+            if test:
+                return {x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}
+            elif validation:
+                return {x: mnist.validation.images, y_: mnist.validation.labels, keep_prob: 1.0}
+            else:
+                return {x: batch[0], y_: batch[1], keep_prob: 0.5}
+
+        if i % 1100 == 0 or i == max_step:
+            checkpoint_file = os.path.join(result_dir, 'checkpoint')
+            saver.save(sess, checkpoint_file, global_step=i)
+
+            test_acc, val_acc = get_accuracies(accuracy, x, y_, keep_prob)
+            print("test accuracy %g" % test_acc)
+            print("validation accuracy %g"% val_acc)
+            summary_str = sess.run(summary_op,feed_dict=feed_dict())
+            train_writer.add_summary(summary_str, i)
+            train_writer.flush()
+
+
+        elif i%100 == 0:
 
             # output the training accuracy every 100 iterations
             train_accuracy = accuracy.eval(feed_dict={
                 x:batch[0], y_:batch[1], keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(i, train_accuracy))
+            # print("step %d, training accuracy %g"%(i, train_accuracy))
 
             # Update the events file which is used to monitor the training (in this case,
             # only the training loss is monitored)
-            summary_str = sess.run(summary_op, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-            summary_writer.add_summary(summary_str, i)
-            summary_writer.flush()
-
-        # save the checkpoints every 1100 iterations
-        if i % 1100 == 0 or i == max_step:
-            checkpoint_file = os.path.join(result_dir, 'checkpoint')
-            saver.save(sess, checkpoint_file, global_step=i)
+            summary_str = sess.run(summary_op, feed_dict=feed_dict())
+            train_writer.add_summary(summary_str, i)
+            train_writer.flush()
 
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5}) # run one train_step
 
