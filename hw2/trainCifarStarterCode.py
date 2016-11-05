a14c7f1bb574d534ad36b8811e34f282b064e0b0
Raymond Cano <rfc1@rice.edu>
2016-11-05 00:56:00 -0700

    initial downloading of starter code

---
+++ hw2/trainCifarStarterCode.py
@@ -0,0 +1,139 @@
+from scipy import misc
+import numpy as np
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+import matplotlib as mp
+
+# --------------------------------------------------
+# setup
+
+def weight_variable(shape):
+    '''
+    Initialize weights
+    :param shape: shape of weights, e.g. [w, h ,Cin, Cout] where
+    w: width of the filters
+    h: height of the filters
+    Cin: the number of the channels of the filters
+    Cout: the number of filters
+    :return: a tensor variable for weights with initial values
+    '''
+
+    # IMPLEMENT YOUR WEIGHT_VARIABLE HERE
+
+    return W
+
+def bias_variable(shape):
+    '''
+    Initialize biases
+    :param shape: shape of biases, e.g. [Cout] where
+    Cout: the number of filters
+    :return: a tensor variable for biases with initial values
+    '''
+
+    # IMPLEMENT YOUR BIAS_VARIABLE HERE
+
+    return b
+
+def conv2d(x, W):
+    '''
+    Perform 2-D convolution
+    :param x: input tensor of size [N, W, H, Cin] where
+    N: the number of images
+    W: width of images
+    H: height of images
+    Cin: the number of channels of images
+    :param W: weight tensor [w, h, Cin, Cout]
+    w: width of the filters
+    h: height of the filters
+    Cin: the number of the channels of the filters = the number of channels of images
+    Cout: the number of filters
+    :return: a tensor of features extracted by the filters, a.k.a. the results after convolution
+    '''
+
+    # IMPLEMENT YOUR CONV2D HERE
+
+    return h_conv
+
+def max_pool_2x2(x):
+    '''
+    Perform non-overlapping 2-D maxpooling on 2x2 regions in the input data
+    :param x: input data
+    :return: the results of maxpooling (max-marginalized + downsampling)
+    '''
+
+    # IMPLEMENT YOUR MAX_POOL_2X2 HERE
+
+    return h_max
+
+
+ntrain =  # per class
+ntest =  # per class
+nclass =  # number of classes
+imsize = 
+nchannels = 
+batchsize = 
+
+Train = np.zeros((ntrain*nclass,imsize,imsize,nchannels))
+Test = np.zeros((ntest*nclass,imsize,imsize,nchannels))
+LTrain = np.zeros((ntrain*nclass,nclass))
+LTest = np.zeros((ntest*nclass,nclass))
+
+itrain = -1
+itest = -1
+for iclass in range(0, nclass):
+    for isample in range(0, ntrain):
+        path = '~/CIFAR10/Train/%d/Image%05d.png' % (iclass,isample)
+        im = misc.imread(path); # 28 by 28
+        im = im.astype(float)/255
+        itrain += 1
+        Train[itrain,:,:,0] = im
+        LTrain[itrain,iclass] = 1 # 1-hot lable
+    for isample in range(0, ntest):
+        path = '~/CIFAR10/Test/%d/Image%05d.png' % (iclass,isample)
+        im = misc.imread(path); # 28 by 28
+        im = im.astype(float)/255
+        itest += 1
+        Test[itest,:,:,0] = im
+        LTest[itest,iclass] = 1 # 1-hot lable
+
+sess = tf.InteractiveSession()
+
+tf_data = #tf variable for the data, remember shape is [None, width, height, numberOfChannels] 
+tf_labels = #tf variable for labels
+
+# --------------------------------------------------
+# model
+#create your model
+
+# --------------------------------------------------
+# loss
+#set up the loss, optimization, evaluation, and accuracy
+
+
+# --------------------------------------------------
+# optimization
+
+sess.run(tf.initialize_all_variables())
+batch_xs = #setup as [batchsize, width, height, numberOfChannels] and use np.zeros()
+batch_ys = #setup as [batchsize, the how many classes] 
+for i in range(): # try a small iteration size once it works then continue
+    perm = np.arange(nsamples)
+    np.random.shuffle(perm)
+    for j in range(batchsize):
+        batch_xs[j,:,:,:] = Train[perm[j],:,:,:]
+        batch_ys[j,:] = LTrain[perm[j],:]
+    if i%10 == 0:
+        #calculate train accuracy and print it
+    optimizer.run(feed_dict={}) # dropout only during training
+
+# --------------------------------------------------
+# test
+
+
+
+
+print("test accuracy %g"%accuracy.eval(feed_dict={tf_data: Test, tf_labels: LTest, keep_prob: 1.0}))
+
+
+sess.close()
