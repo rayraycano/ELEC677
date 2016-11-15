afaf97ac00107c7f627f46919a1bae8de20a34c3
Raymond Cano <rfc1@rice.edu>
2016-11-15 01:56:11 -0600

    finisehd coding, need to experiment and record graphs

--- hw2/lstmMNISTStarterCode.py
+++ hw2/lstmMNISTStarterCode.py
@@ -4,20 +4,27 @@ import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = #call mnist function
+#call mnist function
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-learningRate = 
-trainingIters = 
-batchSize = 
-displayStep = 
+result_dir = './rnn_results/'
+RUN = 'rnn_lrED_128_3e5'
+CELL_TYPE = 'rnn'
 
-nInput = #we want the input to take the 28 pixels
-nSteps = #every 28
-nHidden = #number of neurons for the RNN
-nClasses = #this is MNIST so you know
+global_step = tf.Variable(0, trainable=False)
+learningRate = tf.train.exponential_decay(.001, global_step=global_step, decay_rate=.97, decay_steps=10000)
+# learningRate = .001
+trainingIters = 3e5
+batchSize = 128
+displayStep = 40
 
-x = tf.placeholder('float', [None, nSteps, nInput])
-y = tf.placeholder('float', [None, nClasses])
+nInput = 28 #we want the input to take the 28 pixels
+nSteps = 28 #every 28
+nHidden = 128 #number of neurons for the RNN
+nClasses = 10 #this is MNIST so you know
+
+x_ = tf.placeholder('float', [None, nSteps, nInput])
+y_truth = tf.placeholder('float', [None, nClasses])
 
 weights = {
     'out': tf.Variable(tf.random_normal([nHidden, nClasses]))
@@ -27,50 +34,77 @@ biases = {
     'out': tf.Variable(tf.random_normal([nClasses]))
 }
 
+
 def RNN(x, weights, biases):
     x = tf.transpose(x, [1,0,2])
     x = tf.reshape(x, [-1, nInput])
     x = tf.split(0, nSteps, x) #configuring so you can get it as needed for the 28 pixels
 
-    lstmCell = #find which lstm to use in the documentation
+    if CELL_TYPE == 'rnn':
+        cell = tf.nn.rnn_cell.BasicRNNCell(nHidden)
+    elif CELL_TYPE == 'lstm':
+        cell = tf.nn.rnn_cell.LSTMCell(nHidden)
+    else:
+        cell = tf.nn.rnn_cell.GRUCell(nHidden)
+    outputs, state = tf.nn.rnn(cell, x, dtype=tf.float32)
+    return tf.matmul(outputs[-1], weights['out'] + biases['out'])
 
-    outputs, states = #for the rnn where to get the output and hidden state 
-
-    return tf.matmul(outputs[-1], weights['out'])+ biases['out']
-
-pred = RNN(x, weights, biases)
+pred = RNN(x_, weights, biases)
 
 #optimization
 #create the cost, optimization, evaluation, and accuracy
 #for the cost softmax_cross_entropy_with_logits seems really good
-cost = 
-optimizer = 
+cost = tf.nn.softmax_cross_entropy_with_logits(pred, y_truth, name='softmax_w_logits')
+cross_entropy = tf.reduce_mean(-1 * tf.reduce_sum(y_truth * tf.log(pred), reduction_indices=[1]))
+optimizer = tf.train.AdamOptimizer(learning_rate=learningRate).minimize(cost, global_step=global_step)
 
-correctPred = 
-accuracy = 
 
+correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y_truth, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+tf.scalar_summary(cross_entropy.op.name, cross_entropy)
+summary_op = tf.merge_all_summaries()
 init = tf.initialize_all_variables()
 
+train_file = open(RUN + '_train.txt', 'w')
+index_file = open(RUN + '_index.txt', 'w')
+val_file = open(RUN + "_test.txt", 'w')
 with tf.Session() as sess:
     sess.run(init)
     step = 1
+    sum_writer = tf.train.SummaryWriter(result_dir + RUN, sess.graph)
 
-    while step* batchSize < trainingIters:
-        batchX, batchY = #mnist has a way to get the next batch
+    while step * batchSize < trainingIters:
+        batchX, batchY = mnist.train.next_batch(batch_size=batchSize)#mnist has a way to get the next batch
         batchX = batchX.reshape((batchSize, nSteps, nInput))
 
-        sess.run(optimizer, feed_dict={})
+        summary_string = sess.run(summary_op, feed_dict={x_: batchX, y_truth: batchY})
+        sum_writer.add_summary(summary_string)
+        sum_writer.flush()
 
         if step % displayStep == 0:
-            acc = 
-            loss = 
-            print("Iter " + str(step*batchSize) + ", Minibatch Loss= " + \
-                  "{:.6f}".format() + ", Training Accuracy= " + \
-                  "{:.5f}".format())
+            valX = mnist.validation.images
+            valX = valX.reshape([-1, nSteps, nInput])
+
+            train_cost = cost.eval(session=sess, feed_dict={x_: batchX, y_truth: batchY})
+
+            val_cost = cost.eval(session=sess, feed_dict={x_: valX, y_truth: mnist.validation.labels})
+            print("train cost at " + str(step*batchSize) + "= " + str(np.mean(train_cost)))
+            print("val cost: " + str(np.mean(val_cost)))
+            train_file.write(str(np.mean(train_cost)) + ',')
+            val_file.write(str(np.mean(val_cost)) + ',')
+            index_file.write(str(step*batchSize) + ',')
+            # print("Iter " + str(step*batchSize) + ", Minibatch Loss= " + \
+            #       "{:.6f}".format() + ", Training Accuracy= " + \
+            #       "{:.5f}".format())
+        optimizer.run(feed_dict={x_: batchX, y_truth: batchY})
         step +=1
     print('Optimization finished')
+
+    train_file.close()
+    index_file.close()
+    val_file.close()
 
     testData = mnist.test.images.reshape((-1, nSteps, nInput))
     testLabel = mnist.test.labels
     print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={}))
+        sess.run(accuracy, feed_dict={x_: testData, y_truth:testLabel}))
--- hw2/trainCifarStarterCode.py
+++ hw2/trainCifarStarterCode.py
@@ -1,6 +1,7 @@
 from scipy import misc
 import numpy as np
 import tensorflow as tf
+import os
 import random
 import matplotlib.pyplot as plt
 import matplotlib as mp
@@ -68,6 +69,36 @@ def max_pool_2x2(x):
                            strides=[1,2,2,1], padding='SAME')
     return h_max
 
+def batch_norm(x, n_out, phase_train):
+    """
+    Batch normalization on convolutional maps.
+    Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
+    Args:
+        x:           Tensor, 4D BHWD input maps
+        n_out:       integer, depth of input maps
+        phase_train: boolean tf.Varialbe, true indicates training phase
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    with tf.variable_scope('bn'):
+        beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+                                     name='beta', trainable=True)
+        gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+                                      name='gamma', trainable=True)
+        batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
+        ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(phase_train,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+    return normed
 
 def nn_layer(input_tensor, input_channel, output_channel, window, name):
     """
@@ -84,11 +115,13 @@ def nn_layer(input_tensor, input_channel, output_channel, window, name):
     add_summaries(w_conv, name + '.w')
     b_conv = bias_variable([output_channel])
     add_summaries(b_conv, name + '.b')
-    h_conv = tf.nn.relu(conv2d(input_tensor, w_conv) + b_conv)
+    h_norm = batch_norm(conv2d(input_tensor, w_conv))
+    add_summaries(h_norm, name+'.batch')
+    h_conv = tf.nn.relu(h_norm + b_conv)
     add_summaries(h_conv, name+'.activation')
     pooled = max_pool_2x2(h_conv)
     add_summaries(pooled, name+'.maxpool')
-    return pooled
+    return pooled, w_conv
 
 def add_summaries(tensor, name):
     """
@@ -111,46 +144,58 @@ ntrain = 1000# per class
 ntest = 100# per class
 nclass = 10# number of classes
 imsize = 28
-nchannels = 3
-batchsize = 100
+nchannels = 1
+batchsize = 64
 
 Train = np.zeros((ntrain*nclass,imsize,imsize,nchannels))
 Test = np.zeros((ntest*nclass,imsize,imsize,nchannels))
 LTrain = np.zeros((ntrain*nclass,nclass))
 LTest = np.zeros((ntest*nclass,nclass))
 
-# Load images from folders. 
+# Load images from folders.
 itrain = -1
 itest = -1
+data_dir = './data/CIFAR10/'
 for iclass in range(0, nclass):
     for isample in range(0, ntrain):
-        path = '~/CIFAR10/Train/%d/Image%05d.png' % (iclass,isample)
-        im = misc.imread(path); # 28 by 28
+        path = data_dir + 'Train/%d/Image%05d.png' % (iclass,isample)
+        im = misc.imread(path) # 28 by 28
         im = im.astype(float)/255
         itrain += 1
         Train[itrain,:,:,0] = im
         LTrain[itrain,iclass] = 1 # 1-hot lable
     for isample in range(0, ntest):
-        path = '~/CIFAR10/Test/%d/Image%05d.png' % (iclass,isample)
-        im = misc.imread(path); # 28 by 28
+        path = data_dir + 'Test/%d/Image%05d.png' % (iclass,isample)
+        im = misc.imread(path) # 28 by 28
         im = im.astype(float)/255
         itest += 1
         Test[itest,:,:,0] = im
         LTest[itest,iclass] = 1 # 1-hot lable
+print(LTrain[8500])
+
+def visualize_filter(name, layer):
+    x_min = tf.reduce_min(layer)
+    x_max = tf.reduce_max(layer)
+    kernal_01 = (layer - x_min) / (x_max - x_min)
+
+    # to tf.image_summary fromat
+    kernel_transposed = tf.transpose(kernal_01, [3, 0, 1, 2])
+
+    return tf.image_summary(name, kernal_01, max_images=64)
 
 sess = tf.InteractiveSession()
 
 #tf variable for the data, remember shape is [None, width, height, numberOfChannels]
-tf_data = tf.placeholder(shape=[None, imsize, imsize, nchannels])
+tf_data = tf.placeholder(tf.float32, shape=[None, imsize, imsize, nchannels])
 
 #tf variable for labels
-tf_labels = tf.placeholder(shape=[None, nclass])
+tf_labels = tf.placeholder(tf.float32, shape=[None, nclass])
 
 # --------------------------------------------------
 # model
 #create your model
-layer1 = nn_layer(tf_data, 3, 32, 5, 'layer1')
-layer2 = nn_layer(layer1, 32, 64, 5, 'layer2')
+layer1, filter = nn_layer(tf_data, nchannels, 32, 5, 'layer1')
+layer2, _ = nn_layer(layer1, 32, 64, 5, 'layer2')
 W_fc1 = weight_variable([7 * 7 * 64, 1024], 'wfc1')
 b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(layer2, [-1, 7*7*64])
@@ -168,39 +213,76 @@ y_conv = tf.nn.softmax(h_fc2)
 
 global_step = tf.Variable(0, trainable=False)
 starter_rate = 1e-4
-learning_rate = tf.train.exponential_decay(starter_rate, global_step, 1000, .96, staircase=True)
+learning_rate = tf.train.exponential_decay(starter_rate, global_step, 500, .96, staircase=True)
 
 cross_entropy = tf.reduce_mean(-1 * tf.reduce_sum(tf_labels * tf.log(y_conv), reduction_indices=[1]))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy, global_step=global_step)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(tf_labels, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # --------------------------------------------------
 # optimization
 tf.scalar_summary(cross_entropy.op.name, cross_entropy)
-sess.run(tf.initialize_all_variables())
+
+# Try to do a tf.split() on the filter tensor rather than transposing it
+splits = tf.split(3, 32, filter)
+for i in range(len(splits)):
+    if len(str(i)) == 1:
+        num_str = '0' + str(i)
+    else:
+        num_str = str(i)
+    # tf.image_summary('test', splits)
+
+    tf.image_summary('filter' + num_str, tf.transpose(splits[i], [3, 0, 1, 2]))
+# img_summary = tf.image_summary('filter', splits)
+summary_op = tf.merge_all_summaries()
+
+# Add the variable initializer Op.
+# init = tf.initialize_all_variables()
+
+# Create a saver for writing training checkpoints.
+saver = tf.train.Saver()
+
+# Instantiate a SummaryWriter to output summaries and the Graph.
+result_dir = './results/'
+RUN = 'AdamReluEDViz1'
+train_writer = tf.train.SummaryWriter(result_dir + RUN + '/train', sess.graph)
 
 sess.run(tf.initialize_all_variables())
 
-#setup as [batchsize, width, height, numberOfChannels] and use np.zeros()
-batch_xs = [batchsize, imsize, imsize, nchannels]
-batch_ys = [batchsize, nchannels]#setup as [batchsize, the how many classes]
+sess.run(tf.initialize_all_variables())
 
-for i in range(1000): # try a small iteration size once it works then continue
-    perm = np.arange(ntrain)
+# setup as [batchsize, width, height, numberOfChannels] and use np.zeros()
+batch_xs = np.zeros(shape=[batchsize, imsize, imsize, nchannels])
+
+# setup as [batchsize, the how many classes]
+batch_ys = np.zeros(shape=[batchsize, nclass])
+
+for i in range(2000): # try a small iteration size once it works then continue
+    perm = np.arange(ntrain * nclass)
     np.random.shuffle(perm)
     for j in range(batchsize):
-        batch_xs[j,:,:,:] = Train[perm[j],:,:,:]
-        batch_ys[j,:] = LTrain[perm[j],:]
-    if i%10 == 0:
+        batch_xs[j] = Train[perm[j]]
+        batch_ys[j] = LTrain[perm[j]]
+
+    summary_str = sess.run(summary_op, feed_dict={tf_data: batch_xs, tf_labels:batch_ys, keep_prob:0.5})
+    train_writer.add_summary(summary_str, i)
+    train_writer.flush()
+
+    if i%50 == 0:
         # calculate train accuracy and print it
         train_accuracy = accuracy.eval(feed_dict={
             tf_data: batch_xs, tf_labels: batch_ys, keep_prob: 1.0})
-        print("step %d, training accuracy %g"%(i, train_accuracy))
+        print("step %d, training accuracy %f"%(i, train_accuracy))
+    # if i%100 == 0:
+    #     train_writer.add_summary(img_summary, i)
 
     # dropout only during training
     optimizer.run(feed_dict={tf_data:batch_xs, tf_labels:batch_ys,
                              keep_prob:0.5})
+# Visualize the images in the filter
+
+
 
 # --------------------------------------------------
 # test
